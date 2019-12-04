12c83827ed51dde2867ed8feb48061e96d69a975
donghyukjacobjang <donghyuk0117@gmail.com>
2019-12-04 14:39:35 -0500

    Add .env.local

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.REACT_APP_CUSTOM_NODE_ENV);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
